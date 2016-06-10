2658c3b0b0c4037863486226ac357dde23e7f155
LeslieTanner <lat7dg@virginia.edu>
2016-06-10 14:40:25 -0400

    Added Decision Support Tool Project and Link

---
+++ DecisionSupportTool/data/DroneRanges.js
@@ -0,0 +1,31 @@
+// Drone data...
+var droneRanges = {
+    raven: {
+        range: 12000,
+        strokeColor: "#0000FF",
+        center: {lat: 41.3624701, lng: -73.9777579},
+        lat: 41.36247,
+        lng: -73.9776
+    },
+    puma: {
+        range: 15000,
+        strokeColor: "#FF0000",
+        center: {lat: 41.3624701, lng: -73.9777579},
+        lat: 41.36247,
+        lng: -73.9776
+    },
+    globalhawk: {
+        range: 22778655,
+        strokeColor: "#00FF00",
+        center: {lat: 41.3624701, lng: -73.9777579},
+        lat: 41.36247,
+        lng: -73.9776
+    },
+    shadow: {
+        range: 251058,
+        strokeColor: "#000000",
+        center: {lat: 41.3624701, lng: -73.9777579},
+        lat: 41.36247,
+        lng: -73.9776
+    }
+}  
