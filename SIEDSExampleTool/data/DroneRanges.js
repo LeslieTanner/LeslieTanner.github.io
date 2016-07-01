685a9578404050610e35783fb354033802d0fca3
LeslieTanner <lat7dg@virginia.edu>
2016-06-30 21:06:45 -0400

    Addition of UVA Bay Game visualization and Aerial Grocery delivery system

---
+++ SIEDSExampleTool/data/DroneRanges.js
@@ -0,0 +1,23 @@
+// Drone data...
+var droneRanges = {
+    raven: {
+        range: 12000,
+        strokeColor: "#0000FF",
+        center: {lat: 41.331145, lng: -84.43197}
+    },
+    puma: {
+        range: 15000,
+        strokeColor: "#FF0000",
+        center: {lat: 41.331145, lng: -84.43197}
+    },
+    globalhawk: {
+        range: 22778655,
+        strokeColor: "#00FF00",
+        center: {lat: 41.331145, lng: -84.43197}
+    },
+    shadow: {
+        range: 251058,
+        strokeColor: "#000000",
+        center: {lat: 41.331145, lng: -84.43197}
+    }
+}  
---
+++ UAVGroceryDelivery/data/DroneRanges2.js
@@ -0,0 +1,31 @@
+// Drone data...
+var droneRanges = {
+    raven: {
+        name: "Raven",
+        range: 12000,
+        maxload: 0.40625,
+        strokeFill: "#2C2C2C",
+        strokeColor: "#2C2C2C"
+    },
+    pidgeon: {
+        name: "Pidgeon",
+        range: 80467,
+        maxload: 0.15625,
+        strokeFill: "#549DC2",
+        strokeColor: "#549DC2"
+    },
+    quadcopter: {
+        name: "Quadcopter",
+        range: 19312,
+        maxload: 2,
+        strokeFill: "#E35755",
+        strokeColor: "#E35755"
+    },
+    paperairplane: {
+        name: "Paper Airplane",
+        range: 75,
+        maxload: 0.05,
+        strokeFill: "#36A582",
+        strokeColor: "#36A582"
+    }
+}
